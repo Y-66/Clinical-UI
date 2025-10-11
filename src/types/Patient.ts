b94b192e65aea8fec2b413a1bda9677b0686abd9
yqy <1749288146yqy@gmail.com>
2025-10-10 20:31:55 -0400

    [创建病人类型]

---
+++ src/types/Patient.ts
@@ -0,0 +1,14 @@
+// types.ts
+export type Patient = {
+    fullName: string;
+    email: string;
+    phone: string;
+    dateOfBirth: string;
+    gender: string;
+    insuranceId: string;
+    bloodType: string;
+    address: string;
+    city: string;
+    state: string;
+    zipCode: string;
+};
